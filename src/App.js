4a5132997ad1a6bf956ae70aa0f26e724bd15423
unknown <thalitaandrezza@hotmail.com>
2019-03-28 22:00:51 -0300

    FEAT: components

--- src/App.js
+++ src/App.js
@@ -1,124 +1,23 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+// import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom'
-import BookList from './BookList'
+import Home from './screens/Home/Home'
+import Search from './screens/Search/Search'
 
-const books = [
-  {
-    "id": "01",
-    "url": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-    "name": "To Kill a Mockingbird",
-    "author": "Harper Lee"
-  },
-  {
-    "id": "02",
-    "url": "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-    "name": "1776",
-    "author": "David McCullough"
-  },
-  {
-    "id": "03",
-    "url":"http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-    "name": "Harry Potter and the Sorcerer's Stone",
-    "author": "J.K. Rowling"
-  },
-  {
-    "id": "04",
-    "url": "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-    "name": "The Hobbit",
-    "author": "J.R.R. Tolkien"
-  },
-  {
-    "id":"05",
-    "url":"http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-    "name": "Oh, the Places You'll Go!",
-    "author": "Seuss"
-  },
-  {
-    "id":"06",
-    "url":"http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
-    "name": "The Adventures of Tom Sawyer",
-    "author":"Mark Twain"
-  }
-]
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    books:[],
-
-    showSearchPage: false
-  }
-
-  componentDidMount(){
-    BooksAPI.getAll().then((books) => {
-      this.setState({books})
-    })
-  }
-
   render() {
     return (
-      <div className="app">
-        
-          <Route path="/add" exact render={() => (
-            <div className="search-books">
-              <div className="search-books-bar">
-                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-                <div className="search-books-input-wrapper">
-                  {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                  */}
-                  <input type="text" placeholder="Search by title or author"/>
-
-                </div>
-              </div>
-              <div className="search-books-results">
-                <ol className="books-grid"></ol>
-              </div>
-            </div>
-          )}/>
-          
-        <Route path="/" exact render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  
-                    <BookList books ={books}/>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/add">Add a book</Link>
-            </div>
-          </div>
-          )}/>
-      </div>
-      
+      <Router>
+        <div className="app">
+          <Route exact path='/' component={Home} />
+          <Route exact path='/search' component={Search} />
+        </div>
+      </Router>
     )
   }
 }
